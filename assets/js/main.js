7bc518bf347f6332b37e93e6f5331e8a01a213d2
FedericoCapitani <federico.capitani@outlook.it>
2022-03-25 16:58:09 +0100

    funzione per prendere i dati dall'input

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
 // prendere i dati in input dall'utente
-function prendere_dati_form() {
-    
+function prendere_dati_biglietto() {
+    var full_name = document.getElementById('full_name').value;
+    var km_da_percorrere = document.getElementById('km_da_percorre').value;
+    var età = document.getElementById('età').value;
 }
+
+console.log(full_name, km_da_percorrere, età);
